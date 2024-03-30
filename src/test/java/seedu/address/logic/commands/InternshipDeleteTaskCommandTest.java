12dba3ab2fab69b0318e6a8c65536f14eaebf8e8
jieqiboh <jieqiboh5836@gmail.com>
2024-03-30 14:42:58 +0800

    Resolve checkstyle issues

--- src/test/java/seedu/address/logic/commands/InternshipDeleteTaskCommandTest.java
+++ src/test/java/seedu/address/logic/commands/InternshipDeleteTaskCommandTest.java
@@ -11,6 +11,7 @@ import static seedu.address.testutil.TypicalInternships.getTypicalInternshipData
 import static seedu.address.testutil.TypicalInternships.getTypicalInternships;
 
 import org.junit.jupiter.api.Test;
+
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.InternshipMessages;
 import seedu.address.model.InternshipData;
