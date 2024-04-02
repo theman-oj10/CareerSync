60679f4fb02296480b6e5f0fdf6ef6d47db1fe1e
HM33-Stu <120201372+HM33-Stu@users.noreply.github.com>
2024-04-02 17:17:54 +0800

    Minor fix

--- src/main/java/seedu/address/logic/commands/InternshipAddCommand.java
+++ src/main/java/seedu/address/logic/commands/InternshipAddCommand.java
@@ -43,7 +43,7 @@ public class InternshipAddCommand extends InternshipCommand {
             + PREFIX_LOCATION + " remote "
             + PREFIX_ROLE + " Software Intern";
 
-    public static final String MESSAGE_SUCCESS = "New internship added: %1$s";
+    public static final String MESSAGE_SUCCESS = "New internship added! %1$s";
     public static final String MESSAGE_DUPLICATE_INTERNSHIP = "This internship already exists in the internship list";
 
     private final Internship toAdd;
