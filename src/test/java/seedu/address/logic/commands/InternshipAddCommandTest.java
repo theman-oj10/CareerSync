10adc9e2c864145d2060af49979cc6a059d1ebf9
theman-oj10 <manojnarender@gmail.com>
2024-04-02 12:39:32 +0800

    Update model stub

--- src/test/java/seedu/address/logic/commands/InternshipAddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/InternshipAddCommandTest.java
@@ -161,16 +161,15 @@ public class InternshipAddCommandTest {
         }
 
         @Override
-<<<<<<< HEAD
         public void sortFilteredInternshipList(Comparator<Internship> comparator) {
-=======
+            throw new AssertionError("This method should not be called.");
+        }
         public void setSelectedInternship(Internship internship) {
             throw new AssertionError("This method should not be called.");
         }
 
         @Override
         public ObservableList<Internship> getSelectedInternship() {
->>>>>>> master
             throw new AssertionError("This method should not be called.");
         }
     }
